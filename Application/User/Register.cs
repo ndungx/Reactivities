4ae3d45378d0362a3d45c47390f5f94dc9917dc0
Nguyễn Dũng <dpron12345@gmail.com>
2022-04-24 17:03:32 +0700

    CurrentUser, Login, Register handler now have image

--- Application/User/Register.cs
+++ Application/User/Register.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Errors;
@@ -74,7 +75,7 @@ namespace Application.User
                         DisplayName = user.DisplayName,
                         Token = _jwtGenerator.CreateToken(user),
                         Username = user.UserName,
-                        Image = null,
+                        Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
                     };
                 }
 
