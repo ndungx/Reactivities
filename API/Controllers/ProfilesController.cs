b97ee519a6c6a9b7640b9b6fc9b872b12772d253
Nguyễn Dũng <dpron12345@gmail.com>
2022-04-26 19:11:18 +0700

    add ListActivities api

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Profiles;
 using MediatR;
@@ -8,13 +9,23 @@ namespace API.Controllers
     public class ProfilesController : BaseController
     {
         [HttpGet("{username}")]
-        public async Task<ActionResult<Profile>> Get(string username) {
+        public async Task<ActionResult<Profile>> Get(string username)
+        {
             return await Mediator.Send(new Details.Query { Username = username });
         }
 
         [HttpPut]
-        public async Task<ActionResult<Unit>> Edit(Edit.Command command) {
+        public async Task<ActionResult<Unit>> Edit(Edit.Command command)
+        {
             return await Mediator.Send(command);
+        }
+
+        [HttpGet("{username}/activities")]
+        public async Task<ActionResult<List<UserActivityDTO>>> ListActivities(string username, string predicate)
+        {
+            return await Mediator.Send(
+                new ListActivities.Query { Username = username, Predicate = predicate }
+            );
         }
     }
 }
