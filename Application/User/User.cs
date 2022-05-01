b2e2387d8549db1e2674413917c61606cba8cebb
Nguyễn Dũng <dpron12345@gmail.com>
2022-05-01 18:46:00 +0700

    add field RefreshToken and create ctor

--- Application/User/User.cs
+++ Application/User/User.cs
@@ -1,10 +1,27 @@
+using System.Linq;
+using System.Text.Json.Serialization;
+using Application.Interfaces;
+using Domain;
+
 namespace Application.User
 {
     public class User
     {
+        public User(AppUser user, IJwtGenerator jwtGenerator, string refreshToken)
+        {
+            this.DisplayName = user.DisplayName;
+            this.Token = jwtGenerator.CreateToken(user);
+            this.Username = user.UserName;
+            this.Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url;
+            this.RefreshToken = refreshToken;
+        }
+
         public string DisplayName { get; set; }
         public string Token { get; set; }
         public string Username { get; set; }
         public string Image { get; set; }
+
+        [JsonIgnore]
+        public string RefreshToken { get; set; }
     }
 }
