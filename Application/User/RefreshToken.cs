0f3c0ba6840749302c0fe710a1cd9214a1ab38a2
Nguyễn Dũng <dpron12345@gmail.com>
2022-05-01 18:45:30 +0700

    add RefreshToken handler

---
+++ Application/User/RefreshToken.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Errors;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.User
+{
+    public class RefreshToken
+    {
+        public class Command : IRequest<User>
+        {
+            public string RefreshToken { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, User>
+        {
+            private readonly UserManager<AppUser> _userManager;
+            private readonly IJwtGenerator _jwtGenerator;
+            private readonly IUserAccessor _userAccessor;
+            public Handler(UserManager<AppUser> userManager, IJwtGenerator jwtGenerator, IUserAccessor userAccessor)
+            {
+                this._userAccessor = userAccessor;
+                this._jwtGenerator = jwtGenerator;
+                this._userManager = userManager;
+            }
+
+            public async Task<User> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var user = await _userManager.FindByNameAsync(
+                    _userAccessor.GetCurrentUsername()
+                );
+
+                var oldToken = user.RefreshToken.SingleOrDefault(
+                    x => x.Token == request.RefreshToken
+                );
+
+                if (oldToken != null && !oldToken.IsActive)
+                {
+                    throw new RestException(HttpStatusCode.Unauthorized);
+                }
+
+                if (oldToken != null)
+                {
+                    oldToken.Revoked = DateTime.UtcNow;
+                }
+
+                var newRefreshToken = _jwtGenerator.GenerateRefreshToken();
+                user.RefreshToken.Add(newRefreshToken);
+
+                await _userManager.UpdateAsync(user);
+
+                return new User(user, _jwtGenerator, newRefreshToken.Token);
+            }
+        }
+    }
+}
